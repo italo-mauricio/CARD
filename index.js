519e28349d37266c7b3105c673e116b1c32f48e5
italo <italogithub@gmail.com>
2022-11-20 23:10:13 -0300

    update

--- index.js
+++ index.js
@@ -3,21 +3,18 @@ const form = document.querySelector("#credit-card");
 const cardNumber = document.querySelector("#card-number");
 const cardHolder = document.querySelector("#name-text");
 const cardExpiration = document.querySelector("#valid-thru-text");
-const cardCVV = document.querySelector("#card-text");
+const cardCVV = document.querySelector("#cvv-text");
 
-
-const cardNumberText = document.querySelector(".number-v1");
-const cardHolderText = document.querySelector(".name-v1");
-const cardExpirationText = document.querySelector(".expiration-v1");
-const cardCVVText = document.querySelector(".cvv-v1");
-
+const cardNumberText = document.querySelector(".number-vl");
+const cardHolderText = document.querySelector(".name-vl");
+const cardExpirationText = document.querySelector(".expiration-vl");
+const cardCVVText = document.querySelector(".cvv-vl");
 
 cardNumber.addEventListener("keyup", (e) => {
     if (!e.target.value) {
         cardNumberText.innerText = "1234 5678 9101 1121";
-
-    }else {
-        const valuesOfInput = e.target.value.replaceALL(" ", "");
+    } else {
+        const valuesOfInput = e.target.value.replaceAll(" ", "");
 
         if (e.target.value.length > 14) {
             e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
@@ -28,11 +25,9 @@ cardNumber.addEventListener("keyup", (e) => {
         } else if (e.target.value.length > 4) {
             e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
             cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
-
+        } else {
+            cardNumberText.innerHTML = valuesOfInput
         }
     }
-
-
 })
-
 
