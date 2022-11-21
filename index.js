c25d920159770b6004130f49ce4f4c9717cf4e46
italo <italogithub@gmail.com>
2022-11-20 21:40:17 -0300

    adicionando JS

---
+++ index.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("#credit-card");
+
+const cardNumber = document.querySelector("#card-number");
+const cardHolder = document.querySelector("#name-text");
+const cardExpiration = document.querySelector("#valid-thru-text");
+const cardCVV = document.querySelector("#card-text");
+
+
+const cardNumberText = document.querySelector(".number-v1");
+const cardHolderText = document.querySelector(".name-v1");
+const cardExpirationText = document.querySelector(".expiration-v1");
+const cardCVVText = document.querySelector(".cvv-v1");
+
+
